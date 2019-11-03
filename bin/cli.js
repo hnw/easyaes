5384afb65ce61e54d548b15b70bd3e973b402588
Yoshio HANAWA <y@hnw.jp>
2019-11-04 01:15:14 +0900

    encryptのときだけエコーバックしないようにした | ヘルプメッセージを親切にした / コードを整理して不要なメソッドを消した

--- bin/cli.js
+++ bin/cli.js
@@ -1,52 +1,46 @@
 #!/usr/bin/env node
 
 const yargs = require('yargs')
-      .alias('p', 'password')
+      .usage('Usage:')
+      .usage('  $0 --keygen [--type [aes|blowfish|3des]]')
+      .usage('  $0 --encrypt')
+      .usage('  $0 --decrypt')
+      .describe('keygen', 'Generate password')
+      .describe('encrypt', 'Encrypt mode')
+      .describe('decrypt', 'Decrypt mode')
+      .describe('type', 'Specify encryption algorithm')
+      .describe('password', 'Specify password (for debug)')
+      .string('type')
+      .string('password')
+      .default('type', 'aes')
       .alias('e', 'encrypt')
       .alias('d', 'decrypt')
       .alias('t', 'type')
+      .alias('p', 'password')
+
       .locale('en');
 const argv = yargs.argv;
 
 const crypto = require('crypto');
-const readline=require("readline");
-const EasyAes=require("../easyaes");
+const readline = require("readline");
+const EasyAes = require("../easyaes");
+const {Writable} = require('stream')
+
+const mutableStdout = new Writable({
+  write: function(chunk, encoding, callback) {
+    if (!this.muted) {
+      process.stdout.write(chunk, encoding);
+    }
+    callback();
+  }
+});
 
 const rl=readline.createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: mutableStdout,
   prompt: "",
-  terminal: false
+  terminal: true
 });
-
-let password = argv.password || EasyAes.getPasswordFromFile();
-
-const mainLoop = (password) => {
-  cipher = new EasyAes(password);
-  rl.on("line", (str) => {
-    if (argv.decrypt) {
-      const test = /(^|[^A-Za-z0-9/])(([A-Za-z0-9/+]{4})*[A-Za-z0-9/+]{2}==)/g;
-      const decrypted = str.replace(test, (_, p1, p2) => p1 + cipher.decrypt(p2));
-      console.log(decrypted);
-    } else {
-      console.log(cipher.encrypt(str));
-    }
-  });
-};
-
-const getPasswordThenLoop = () => {
-  process.stdin.setRawMode(true);
-  rl.question('Password: ', (str) => {
-    if (str !== '') {
-      process.stdin.setRawMode(false);
-      console.log();
-      mainLoop(str);
-    } else {
-      console.log();
-      getPasswordThenLoop();
-    }
-  });
-};
 
 const generateKey = (algorithm) => {
   const pool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#%+,-./:=@^_~';
@@ -64,15 +58,28 @@ const generateKey = (algorithm) => {
   console.log(key);
 }
 
+const password = argv.password || EasyAes.getPasswordFromFile();
+
 if (argv.keygen) {
   generateKey(argv.type);
   process.exit(0);
 } else if (password === '') {
-  if (process.stdin.isTTY) {
-    getPasswordThenLoop();
-  } else {
-    throw new Error('You must specify password when you use pipe/redirect.');
-  }
+  console.error('Password not found. Try "$(npm bin)/easyaes --keygen > $HOME/.easyaes"');
+  process.exit(1);
 } else {
-  mainLoop(password);
+  const cipher = new EasyAes(password);
+  if (argv.decrypt) {
+    const test = /(^|[^A-Za-z0-9/])(([A-Za-z0-9/+]{4})*[A-Za-z0-9/+]{2}==)/g;
+    rl.on("line", (str) => {
+      console.log(str.replace(test, (_, p1, p2) => p1 + cipher.decrypt(p2)));
+    });
+  } else if (argv.decrypt) {
+    mutableStdout.muted = true;
+    rl.on("line", (str) => {
+      console.log(cipher.encrypt(str));
+    });
+  } else {
+    yargs.showHelp()
+    process.exit(1);
+  }
 }
